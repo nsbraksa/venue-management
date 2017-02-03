67d2a84b22ef551157933f8e2a37e49c92a043cf
thegreyfellow <youssef.idmoussi@gmail.com>
2017-02-03 13:12:47 +0000

    Add User model helpers, and check for user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
   resourcify
-  rolify :role_cname => 'Client'
-  rolify :role_cname => 'Manager'
+  rolify role_cname: 'Client'
+  rolify role_cname: 'Manager'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :rooms
   has_many :bookings
+
+  def manager?
+    has_role? :manager
+  end
+
+  def client?
+    has_role? :client
+  end
 end
