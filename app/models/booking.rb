dee65e10eca58ccbb6448dc254031cec27aeb0bd
thegreyfellow <youssef.idmoussi@gmail.com>
2017-02-04 20:15:01 +0000

    Add User cannot book with a datetime, past current datetime

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,8 @@
 # Booking model
 class Booking < ApplicationRecord
   validate :dates_in_order,
-           :booking_available
+           :booking_available,
+           :check_start_time
 
   belongs_to :user
   belongs_to :room
@@ -12,11 +13,15 @@ class Booking < ApplicationRecord
   end
 
   def dates_in_order
-    errors.add(:start_date, 'must be before end_date') unless start_date < end_date
+    errors.add(:start_date, 'must be before end_date.') unless start_date < end_date
   end
 
   def booking_available
     room = Room.find(room_id)
     errors.add(:start_date, ' and End date, are in conflict with some other booking, choose another time.') unless room.available(start_date, end_date)
+  end
+
+  def check_start_time
+    errors.add(:start_date, 'must be after current time.') unless start_date > Time.current
   end
 end
