cd5ce24a9577fd706193270dbb8255ba5a074df1
thegreyfellow <youssef.idmoussi@gmail.com>
2017-02-04 15:59:47 +0000

    Add booking start_date should be lesser than end_date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,16 @@
 # Booking model
 class Booking < ApplicationRecord
+  validate :dates_in_order
+
   belongs_to :user
   belongs_to :room
 
   def room_name
     room = room_id
     Room.find(room).name
+  end
+
+  def dates_in_order
+    errors.add(:start_date, 'must be before end_date') unless start_date < end_date
   end
 end
